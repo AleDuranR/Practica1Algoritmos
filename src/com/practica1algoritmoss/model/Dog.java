9f0d49b73b3f2d6f3ae214dfb7349478c6d2e889
AleDuranR <aduranrocha@gmail.com>
2019-10-10 13:14:21 -0500

    ADD two properties int

--- src/com/practica1algoritmoss/model/Dog.java
+++ src/com/practica1algoritmoss/model/Dog.java
@@ -1,10 +1,6 @@
 package com.practica1algoritmoss.model;
 
 public class Dog {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	private int idDog;
+	private int name;
 }
