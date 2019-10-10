feb473850b7ecf5ea713a005b03ecac510be0246
AleDuranR <aduranrocha@gmail.com>
2019-10-10 12:51:24 -0500

    Fist commit of mi project

---
+++ src/com/practica1algoritmoss/app/HolaMundo.java
@@ -0,0 +1,9 @@
+package com.practica1algoritmoss.app;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.print("Hola mundo");		//TODO me llamo Ale y este es mi primer programa en JAVA
+	}
+
+}
